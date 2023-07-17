6a4c73b74c94221a93f67bc4c44d900b3e78b98d
Panxuc <panxuc0618@gmail.com>
2023-07-18 00:13:35 +0800

    基本实现功能（？）

--- calculator/MainPage.xaml.cs
+++ calculator/MainPage.xaml.cs
@@ -11,11 +11,22 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
+    public enum ButtonType
+    {
+        DEFAULT,
+        NUMBER,
+        OPERATOR,
+        EQUAL,
+        UNARY
+    }
+
     // 定义一些变量来存储当前输入的数字，当前选择的运算符，以及上一次计算的结果
     private double currentNumber = 0;
     private double lastNumber = 0;
     private string currentOperator = "";
     private bool isResult = false;
+    private ButtonType lastButton = ButtonType.DEFAULT;
+    public bool isAdvancedMode = false;
 
     // 定义OnNumberClicked方法来处理数字按钮点击事件
     private void OnNumberClicked(object sender, EventArgs e)
@@ -24,18 +35,36 @@ public partial class MainPage : ContentPage
         var button = sender as Button;
         var number = button.Text;
 
-        // 如果当前显示的是结果，或者是0，就清空显示屏
-        if (isResult || displayLabel.Text == "0")
+        if (number != "π" && number != "e")
         {
-            displayLabel.Text = "";
-            if (number == ".")
-                displayLabel.Text = "0";
-            isResult = false;
+            // 如果当前显示的是结果，或者是0，就清空显示屏
+            if (isResult || displayLabel.Text == "0")
+            {
+                displayLabel.Text = "";
+                if (number == ".")
+                    displayLabel.Text = "0";
+                isResult = false;
+            }
+
+            // 将数字追加到显示屏，并更新当前输入的数字
+            displayLabel.Text += number;
+            currentNumber = double.Parse(displayLabel.Text);
+        }
+        else
+        {
+            if (isResult || displayLabel.Text == "0")
+            {
+                currentNumber = (number == "π" ? Math.PI : Math.E);
+                displayLabel.Text = currentNumber.ToString();
+            }
+            else
+            {
+                currentNumber *= (number == "π" ? Math.PI : Math.E);
+                displayLabel.Text = currentNumber.ToString();
+            }
         }
 
-        // 将数字追加到显示屏，并更新当前输入的数字
-        displayLabel.Text += number;
-        currentNumber = double.Parse(displayLabel.Text);
+        lastButton = ButtonType.NUMBER;
     }
 
     // 定义OnOperatorClicked方法来处理运算符按钮点击事件
@@ -45,23 +74,56 @@ public partial class MainPage : ContentPage
         var button = sender as Button;
         var op = button.Text;
 
+        switch (op)
+        {
+            case "lg":
+            case "ln":
+            case "√":
+            case "!":
+            case "sin":
+            case "cos":
+            case "tan":
+                lastButton = ButtonType.UNARY;
+                break;
+            default:
+                break;
+        }
+
         // 如果当前的运算符不为空，就执行上一次选择的运算，并显示结果
         if (currentOperator != "")
         {
-            Calculate();
-            displayLabel.Text = lastNumber.ToString();
-            isResult = true;
+            if (lastButton == ButtonType.NUMBER || lastButton == ButtonType.UNARY)
+            {
+                Calculate();
+                displayLabel.Text = lastNumber.ToString();
+                isResult = true;
+            }
+            else
+            {
+                currentOperator = op;
+            }
         }
         else
         {
             // 否则，就将当前输入的数字赋值给上一次计算的结果
             lastNumber = currentNumber;
-            displayLabel.Text = "0";
-            isResult = false;
+            if (lastButton == ButtonType.UNARY)
+            {
+                currentOperator = op;
+                Calculate();
+                displayLabel.Text = lastNumber.ToString();
+                isResult = true;
+            }
+            else
+            {
+                displayLabel.Text = "0";
+                isResult = false;
+            }
         }
 
         // 将当前选择的运算符赋值给变量，并清空当前输入的数字
         currentOperator = op;
+
     }
 
     // 定义OnEqualClicked方法来处理等号按钮点击事件
@@ -75,6 +137,8 @@ public partial class MainPage : ContentPage
             isResult = true;
             currentOperator = "";
         }
+
+        lastButton = ButtonType.EQUAL;
     }
 
     // 定义OnEqualClicked方法来处理等号按钮点击事件
@@ -85,6 +149,47 @@ public partial class MainPage : ContentPage
         currentOperator = "";
         isResult = false;
         displayLabel.Text = lastNumber.ToString();
+
+        lastButton = ButtonType.DEFAULT;
+    }
+
+    // 定义OnDeleteClicked方法来处理删除按钮点击事件
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        // 如果当前显示的是结果，就清空显示屏
+        if (isResult && lastButton == ButtonType.EQUAL)
+        {
+            displayLabel.Text = "0";
+            isResult = false;
+        }
+        else if (lastButton == ButtonType.NUMBER)
+        {
+            // 否则，就删除最后一个字符，并更新当前输入的数字
+            displayLabel.Text = displayLabel.Text.Remove(displayLabel.Text.Length - 1);
+            if (displayLabel.Text == "")
+                displayLabel.Text = "0";
+            currentNumber = double.Parse(displayLabel.Text);
+        }
+        else if(lastButton == ButtonType.OPERATOR)
+        {
+            currentOperator = "";
+        }
+    }
+
+    // 定义OnSwitchClicked方法来处理复杂计算模式切换按钮点击事件
+    private void OnSwitchClicked(object sender, EventArgs e)
+    {
+        standard.IsVisible = !standard.IsVisible;
+        scientific.IsVisible = !scientific.IsVisible;
+    }
+
+    // 定义Factorial方法来计算阶乘
+    private int Factorial(int n)
+    {
+        if (n == 0)
+            return 1;
+        else
+            return n * Factorial(n - 1);
     }
 
     // 定义Calculate方法来执行运算逻辑
@@ -104,6 +209,30 @@ public partial class MainPage : ContentPage
                 break;
             case "/":
                 lastNumber /= currentNumber;
+                break;
+            case "^":
+                lastNumber = Math.Pow(lastNumber, currentNumber);
+                break;
+            case "lg":
+                lastNumber = Math.Log10(lastNumber);
+                break;
+            case "ln":
+                lastNumber = Math.Log(lastNumber);
+                break;
+            case "√":
+                lastNumber = Math.Sqrt(lastNumber);
+                break;
+            case "!":
+                lastNumber = Factorial((int)lastNumber);
+                break;
+            case "sin":
+                lastNumber = Math.Sin(lastNumber);
+                break;
+            case "cos":
+                lastNumber = Math.Cos(lastNumber);
+                break;
+            case "tan":
+                lastNumber = Math.Tan(lastNumber);
                 break;
             default:
                 break;
